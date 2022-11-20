d8cebdbf9197c8d3a4f5d888380bfaf0708e7dca
onemanatatime <8282076+onemanatatime@users.noreply.github.com>
2022-11-20 19:12:53 +0800

    feat: working basic skeleton

--- contracts/game.md
+++ contracts/game.md
@@ -2,11 +2,37 @@
 
 ## Contract address
 
-https://mumbai.polygonscan.com/address/0x95d2205b7539aa693499471cd94383d181d4408a
+TX: https://mumbai.polygonscan.com/tx/0x6887d2c0514fa51671c63aaac54a8732dcb3b1eb5d06bd311fc697c413d74686
 
-### Battle transaction
+Contract Address: https://mumbai.polygonscan.com/address/0x4174a2635cd36c1730a1238d6eb0b6a4aa36f9ac
 
-https://mumbai.polygonscan.com/tx/0xefe1b1798ac486e2e53ac77a9346720744591dc1b92882c5b14305509330e307
 
-### After 1 battle
-![Adventurer game after 1 battle](https://user-images.githubusercontent.com/8282076/202893225-5e553cd4-bdbc-431a-9d7d-7b61717dfe25.png)
+## Verify contract
+
+## Playing the game
+
+### 1. mapPlayer()
+
+TX: https://mumbai.polygonscan.com/tx/0x626a426d808e21bb0a62754482e6b4a59008f48348ffa05301e7596191490474
+
+### 2. mapSpawn()
+
+TX: https://mumbai.polygonscan.com/tx/0xf061e9ac8d0041c03ad6b335f9a03f02b89d778bd739bc206dfc7bf0a634ed61
+
+### 3. battle(_index)
+
+TX: https://mumbai.polygonscan.com/tx/0x2f4433d5dd2ad61367597655c3d50409e7b5a50d43efb57f89ba5afcab22994f
+
+### View results
+
+#### getHero(index)
+
+![Adventurer stats after 1 battle](https://user-images.githubusercontent.com/8282076/202898827-cbb17b2f-8d5e-468e-af24-15d939fa0516.png)
+
+#### getSpawn(index)
+
+![Spawn stats after 1 battle](https://user-images.githubusercontent.com/8282076/202898840-808de1c9-a21c-4723-bafe-67aaac8ade4a.png)
+
+#### getScore()
+
+![Game Score after 1 battle](https://user-images.githubusercontent.com/8282076/202898835-ce0a9200-68fe-4dd4-bbe5-553b980acaaf.png)
--- contracts/game.sol
+++ contracts/game.sol
@@ -3,56 +3,128 @@ pragma solidity ^0.8.0;
 
 contract Game {
 
-    int score;
+    // uint score;
 
-    struct Person { // only to define the structure; not to define any values to elements;
+    struct Hero { // only to define the structure; not to define any values to elements;
         string name;
-        uint health;
-        uint strength;
-        uint agility;
-        uint wisdom;
+        int health;
+        int strength;
+        int agility;
+        int wisdom;
     }
 
-    Person[] people;
+    struct Spawn {
+        int health;
+        int strength;
+        int agility;
+        int wisdom;
+    }
 
-    function addPerson(string memory _name, uint _health, uint _strength, uint _agility, uint _wisdom) public {
-        Person memory newPerson = Person(_name, _health, _strength, _agility, _wisdom); // Create an `instance` of `Person`
-        people.push(newPerson);
+    struct Round {
+        uint roundNo;
+        bool result;
+        uint coinsWon;
+    }
+
+    Hero[] players;
+    Spawn[] spawns;
+    Round [] rounds;
+
+    function addHero(string memory _name, int _health, int _strength, int _agility, int _wisdom) private {
+        Hero memory newHero = Hero(_name, _health, _strength, _agility, _wisdom); // Create an `instance` of `Hero`
+        players.push(newHero);
     } 
 
-    function getPerson(uint _index) public view returns (string memory, uint, uint, uint, uint) {
-        Person memory personToReturn = people[_index];
-        return (personToReturn.name, personToReturn.health, personToReturn.strength, personToReturn.agility, personToReturn.wisdom);
-    }
+    function addSpawn(int _health, int _strength, int _agility, int _wisdom) private {
+        Spawn memory newSpawn = Spawn(_health, _strength, _agility, _wisdom);
+        spawns.push(newSpawn);
+    } 
+
+    function addRound(uint _roundNo, bool _result, uint _score) private {
+        Round memory newRound = Round(_roundNo, _result, _score);
+        rounds.push(newRound);
+    } 
+
+    // TODO: generate pseudo-random nos for attributes
+
+    // function getStrength(uint256 tokenId) public view returns (string memory) {
+    //     return pluck(tokenId, "STRENGTH", strength);
+    // }
     
-    // function hello() public pure returns(string memory) {
-    //     string memory message = "Hello World!"; 
-    //     return message;
+    // function getAgility(uint256 tokenId) public view returns (string memory) {
+    //     return pluck(tokenId, "AGILITY", agility);
+    // }
+    
+    // function getWisdom(uint256 tokenId) public view returns (string memory) {
+    //     return pluck(tokenId, "WISDOM", wisdom);
     // }
 
-    function battle() public returns (string memory) {
-        // Person[] memory people; // Create `Person[]` type for variable `people`
+    // function pluck(uint256 tokenId, string memory keyPrefix, string[] memory sourceArray) internal pure returns (string memory) {
+    //     uint256 rand = random(string(abi.encodePacked(keyPrefix, toString(tokenId))));
+    //     string memory output = sourceArray[rand % sourceArray.length];
+    //     return output;
+    // }
 
-        addPerson("Hero", 100, 10, 9, 8);
-        addPerson("Enemy", 100, 7, 6, 5);
+    function getHero(uint _index) public view returns (string memory, int, int, int, int) {
+        Hero memory heroToReturn = players[_index];
+        return (heroToReturn.name, heroToReturn.health, heroToReturn.strength, heroToReturn.agility, heroToReturn.wisdom);
+    }
 
-        if (people[0].health > 0 && people[1].health > 0){
+    function getSpawn(uint _index) public view returns (int, int, int, int) {
+        Spawn memory spawnToReturn = spawns[_index];
+        return (spawnToReturn.health, spawnToReturn.strength, spawnToReturn.agility, spawnToReturn.wisdom);
+    }
 
-            people[0].health -= 7;
-            people[1].health -= 10;
+    function getRound(uint _index) public view returns (uint, bool, uint) {
+        Round memory roundToReturn = rounds[_index];
+        return (roundToReturn.roundNo, roundToReturn.result, roundToReturn.coinsWon);
+    }
 
+    // function getScore() public view returns(uint){
+    //     return score;
+    // }
+
+    function mapPlayer(string memory _name, int _str, int _agi, int _wis) public returns (uint) {
+        addHero(_name, 100, _str, _agi, _wis);
+        return players.length-1; // return index of newly mapped player
+    }
+
+    function mapSpawn(int _str, int _agi, int _wis) public returns (uint) {
+        addSpawn(100, _str, _agi, _wis);
+        return spawns.length-1; // return index of newly mapped spawn
+    }
+    
+    function battle(uint _index) public {
+        // players[_index].health;
+        // players[_index].strength;
+        // players[_index].agility;
+        // players[_index].wisdom;
+
+        // spawns[_index].health;
+        // spawns[_index].strength;
+        // spawns[_index].agility;
+        // spawns[_index].wisdom;
+
+        // addRound(_index, false);
+        // rounds[_index].roundNo;
+
+        // start game
+        while (players[_index].health > 0 && spawns[_index].health > 0){
+            players[_index].health -= spawns[_index].strength;
+            spawns[_index].health -= players[_index].strength;
         }
-        score++;
-        string memory message = "Game ended!";
 
-        people[0].health = 100;
-        people[1].health = 100;
+        // // check winner
+        // if (players[_index].health < 0 && spawns[_index].health < 0){
+        //     addRound(_index, false, 0);
+        // } else if (players[_index].health > 0 && spawns[_index].health <= 0){
+        //     addRound(_index, true, 10);
+        //     // score++;
+        // } else if (players[_index].health <= 0 && spawns[_index].health > 0){
+        //     addRound(_index, false, 10);
+        // }
 
-        return message;
+        addRound(_index, true, 10);
+
     }
-
-    function getScore() public view returns(int){
-        return score;
-    }
-
 }
