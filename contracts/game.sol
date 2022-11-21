5934f23aba20e63b85495422bab802e3488bc48e
onemanatatime <8282076+onemanatatime@users.noreply.github.com>
2022-11-21 15:50:15 +0800

    clean up done/unused

--- contracts/game.sol
+++ contracts/game.sol
@@ -9,7 +9,6 @@ contract Game {
         int strength;
         int agility;
         int wisdom;
-        // add pray() function; // TODO
     }
 
     struct Spawn {
@@ -43,31 +42,7 @@ contract Game {
     Round[] rounds;
     Attack[] attacks;
 
-    int[] attributesArray = [
-        1,
-        2,
-        3,
-        4,
-        5,
-        6,
-        7,
-        8,
-        9,
-        10
-    ];
-
-    int[] randomizerArray = [
-        1,2,3,4,5,6,7,8,9,10,
-        11,12,13,14,15,16,17,18,19,20,
-        21,22,23,24,25,26,27,28,29,30,
-        31,32,33,34,35,36,37,38,39,40,
-        41,42,43,44,45,46,47,48,49,50,
-        51,52,53,54,55,56,57,58,59,60,
-        61,62,63,64,65,66,67,68,69,70,
-        71,72,73,74,75,76,77,78,79,80,
-        81,82,83,84,85,86,87,88,89,90,
-        91,92,93,94,95,96,97,98,99,100
-    ];
+    int[] attributesArray = [1,2,3,4,5,6,7,8,9,10];
 
     function addHero(string memory _name, int _health, int _strength, int _agility, int _wisdom) private {
         Hero memory newHero = Hero(_name, _health, _strength, _agility, _wisdom);
