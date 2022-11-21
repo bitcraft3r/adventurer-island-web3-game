a93eae06f09cfdcfaef484867e8ef584fca0f24b
onemanatatime <8282076+onemanatatime@users.noreply.github.com>
2022-11-21 10:32:42 +0800

    feat: add critical hit and dodge (bugged)

--- contracts/game.md
+++ contracts/game.md
@@ -2,9 +2,9 @@
 
 ## Contract address
 
-TX: https://mumbai.polygonscan.com/tx/0xbb469a6e199f40a4c642cde5a97ea9f2aa9a7a9da663adafbb9e780922d69b99
+TX: https://mumbai.polygonscan.com/tx/0xb7617031b95ad512ab70f239c64fe7d34457add0407b102805528b4fce65d8b2
 
-Contract Address: https://mumbai.polygonscan.com/address/0xa504ddbaf3794fab08a695e408d8e88743f5be10#readContract
+Contract Address: https://mumbai.polygonscan.com/address/0x166992350969c893577bfa6649bf0d4a10ccc071#readContract
 
 ## Verify contract
 
@@ -12,30 +12,45 @@ Contract Address: https://mumbai.polygonscan.com/address/0xa504ddbaf3794fab08a69
 
 ### 2. battle(_index)
 
-TX: https://mumbai.polygonscan.com/tx/0x9a09587e9a44e4b8bf8bd829fd4a7d678a3475c22d1119faf4db9e2ee607cf9f
+TX: https://mumbai.polygonscan.com/tx/0x76284a49e200ba114584fa9a7b32d5cd0f58f3f48fe235066a12a60438e0d5f4
 
 ### View results
 
 #### WIN case
 
-![Adventurer stats after 1 battle](https://user-images.githubusercontent.com/8282076/202936253-7aa3c488-a8cb-4eff-ac37-3245eabff03a.png)
+![Adventurer stats after 1 battle](https://user-images.githubusercontent.com/8282076/202947219-26af0318-e155-44da-add3-55a690b30dc4.png)
 
-![Spawn stats after 1 battle](https://user-images.githubusercontent.com/8282076/202936262-438d7789-53aa-4c0b-9297-3ef6ba42c54b.png)
+![Spawn stats after 1 battle](https://user-images.githubusercontent.com/8282076/202947318-2d997913-72a4-446a-8135-ece76c634390.png)
 
-![Game Score after 1 battle](https://user-images.githubusercontent.com/8282076/202936272-a12c696e-fd6c-4bb7-9c77-c31580d6bc65.png)
+![Game Score after 1 battle](https://user-images.githubusercontent.com/8282076/202947388-7b157643-0099-446a-9f4d-2e79bc7ed58c.png)
 
+#### BUG?
+
+For Index = 4,
+Player was left with 100 HP.
+
+![Bug? Player and Spawn stats after 1 battle](https://user-images.githubusercontent.com/8282076/202949859-12e7c467-1ac3-485b-a2d6-23cd351563d3.png)
+
+
+Contract broke after Index = 6?
+Can't create index=7:
+
+![Bug? Cannot create the next game](https://user-images.githubusercontent.com/8282076/202950722-6a143776-64dd-4818-9d36-889c58b551ad.png)
+
+
+<!-- 
 #### LOSE case
 
-![Adventurer stats after 1 battle](https://user-images.githubusercontent.com/8282076/202936052-13676b4e-6ccb-4388-b4a6-4e773fa0e759.png)
+![Adventurer stats after 1 battle]()
 
-![Spawn stats after 1 battle](https://user-images.githubusercontent.com/8282076/202936064-ec686839-ae8d-4c20-b3d6-56e5bd1c8979.png)
+![Spawn stats after 1 battle]()
 
-![Game Score after 1 battle](https://user-images.githubusercontent.com/8282076/202936075-b620f64a-5e43-4ea2-a70a-61990677c72d.png)
+![Game Score after 1 battle]()
 
 #### TIE case
 
-![Adventurer stats after 1 battle](https://user-images.githubusercontent.com/8282076/202936131-8a268562-ba15-42b1-809d-96aa17cfe670.png)
+![Adventurer stats after 1 battle]()
 
-![Spawn stats after 1 battle](https://user-images.githubusercontent.com/8282076/202936142-d6b0e789-458c-4170-8203-6bf1444e0a82.png)
+![Spawn stats after 1 battle]()
 
-![Game Score after 1 battle](https://user-images.githubusercontent.com/8282076/202936153-624f295e-e6cd-476e-b44f-2bf4c479ea00.png)
+![Game Score after 1 battle]() -->
--- contracts/game.sol
+++ contracts/game.sol
@@ -24,6 +24,14 @@ contract Game {
         uint coinsWon;
     }
 
+    // struct Attacks {
+    //     uint attackNo;
+    //     uint 
+    // }
+
+    // uint attackCounter = 0;
+    // // attackCounter++ in the attacks
+
     Hero[] players;
     Spawn[] spawns;
     Round[] rounds;
@@ -39,6 +47,19 @@ contract Game {
         8,
         9,
         10
+    ];
+
+    int[] randomizerArray = [
+        1,2,3,4,5,6,7,8,9,10,
+        11,12,13,14,15,16,17,18,19,20,
+        21,22,23,24,25,26,27,28,29,30,
+        31,32,33,34,35,36,37,38,39,40,
+        41,42,43,44,45,46,47,48,49,50,
+        51,52,53,54,55,56,57,58,59,60,
+        61,62,63,64,65,66,67,68,69,70,
+        71,72,73,74,75,76,77,78,79,80,
+        81,82,83,84,85,86,87,88,89,90,
+        91,92,93,94,95,96,97,98,99,100
     ];
 
     function addHero(string memory _name, int _health, int _strength, int _agility, int _wisdom) private {
@@ -128,10 +149,40 @@ contract Game {
         // initialize spawn
         addSpawn(100, getStrength(currentIndex, _name), getAgility(currentIndex, _name), getWisdom(currentIndex, _name));
 
+        // variables
+        string memory dodgeString = "DODGED! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugiat voluptate autem minima magnam quisquam neque adipisci, doloribus dolores modi porro quam tenetur in reiciendis corrupti ea iure laudantium expedita nesciunt eaque nemo! Animi adipisci, veniam voluptatibus cum natus enim perferendis accusantium expedita dolores laborum perspiciatis blanditiis rerum et quasi quo reiciendis recusandae esse est excepturi aliquid! Iste quos vero aperiam velit explicabo tempore laudantium eaque voluptatibus ab dolorum amet laborum fugiat repellat nulla qui veritatis assumenda, delectus libero consectetur aliquam, inventore deserunt repudiandae. Unde id eius hic aspernatur, quasi nemo qui nam quis. Accusamus, pariatur? Nihil ad laudantium consectetur alias!";
+        string memory criticalHitString = "CRITICAL HIT! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Est possimus aspernatur quidem quae ipsam tempora dolor ex quam, porro similique alias reprehenderit, id vero, facilis quisquam! Eum adipisci animi culpa id eos! Eaque velit, labore tempora quasi ex voluptate sapiente fugit, saepe, aut doloremque ipsum aliquam eveniet unde quidem? Aspernatur adipisci temporibus, fuga nesciunt nam quod neque dolores vitae amet id suscipit similique quis. Repudiandae consectetur, unde repellat odio reprehenderit voluptates at atque, rem quasi perspiciatis illo. Vero consequuntur eius assumenda expedita, culpa reiciendis exercitationem praesentium nihil dolores. Minima consequuntur quam, odio sint sapiente non exercitationem! Beatae quaerat dolores incidunt!";
+
         // start battle
         while (players[currentIndex].health > 0 && spawns[currentIndex].health > 0){
-            players[currentIndex].health -= spawns[currentIndex].strength;
-            spawns[currentIndex].health -= players[currentIndex].strength;
+            // player attacks
+            // 2%-25% chance for spawn to dodge
+            if (4*pluck(currentIndex, "DODGE", randomizerArray, dodgeString) <= 10*spawns[currentIndex].agility){
+                // dodge
+            } else {
+            // 2%-25% chance for player to deal critical damage
+                if (4*pluck(currentIndex, "CRITICALHIT", randomizerArray, criticalHitString) <= 10*players[currentIndex].wisdom){
+                    // attack w critical hit
+                    spawns[currentIndex].health -= 5 + players[currentIndex].strength - spawns[currentIndex].agility/2 + 5 + getStrength(currentIndex, _name);
+                } else {
+                    // attack w/o critical hit
+                    spawns[currentIndex].health -= 5 + players[currentIndex].strength - spawns[currentIndex].agility/2;
+                }
+            }
+            // spawn attacks
+            // 2%-25% chance for player to dodge
+            if (4*pluck(currentIndex, "DODGE", randomizerArray, dodgeString) <= 10*players[currentIndex].agility){
+                // dodge
+            } else {
+            // 2%-25% chance for spawn to deal critical damage
+                if (4*pluck(currentIndex, "CRITICALHIT", randomizerArray, criticalHitString) <= 10*spawns[currentIndex].wisdom){
+                    // attack w critical hit
+                    players[currentIndex].health -= 5 + spawns[currentIndex].strength - players[currentIndex].agility/2 + 5 + getStrength(currentIndex, _name);
+                } else {
+                    // attack w/o critical hit
+                    players[currentIndex].health -= 5 + spawns[currentIndex].strength - players[currentIndex].agility/2;
+                }
+            }
         }
 
         // check winner
