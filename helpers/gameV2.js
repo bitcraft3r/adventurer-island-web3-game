399ced1c55bacb55b2e3f8b864ac56536a32727f
onemanatatime <8282076+onemanatatime@users.noreply.github.com>
2022-11-27 12:48:15 +0800

    simulate full battle

--- helpers/gameV2.js
+++ helpers/gameV2.js
@@ -6,26 +6,35 @@ class Character {
         this.strength = 1;
         this.agility = 1;
         this.wisdom = 1;
+        this.damage = 1;
     }
-    attack(){
-        
+    attack(defender){
+        defender.hitpoints -= this.damage;
     }
 }
 
 class Spawn extends Character {
-    consturctor(){
+    constructor(){
         super();
+        this.strength = this.generateStat(10);
+        this.agility = this.generateStat(10);
+        this.wisdom = this.generateStat(10);
+        this.damage = 5 + this.strength;
         this.drops = [];
         this.gold = 1;
+    }
+    generateStat(number){
+        return Math.floor(Math.random()*number) + 1; // returns 1 to number; enter value above 1
     }
     transmogrify(){
         // if hero has unfair advantage, give spawn bonus stats
     }
 }
 
-class Hero extends Character {
-    consturctor(){
+class Adventurer extends Character {
+    constructor(){
         super();
+        this.damage = 5;
         this.bag = [];
         this.gold = 0;
         this.skills = {};
@@ -41,14 +50,50 @@ class Hero extends Character {
     }
 }
 
-class Warrior extends Hero {}
-class Archer extends Hero {}
-class Wizard extends Hero {}
+class Warrior extends Adventurer {
+    constructor(name, gameClass, str, agi, wis){
+        super(name);
+        this.name = name; // use DOM input by user
+        this.gameClass = gameClass; // use DOM input by user
+        this.strength = str + 1; // use DOM input retrieved from NFT metadata // get bonus +1 str for warrior
+        this.agility = agi; // use DOM input retrieved from NFT metadata
+        this.wisdom = wis; // use DOM input retrieved from NFT metadata
+        this.id = "Adventurer #2"; // use DOM input retrieved from NFT metadata
+        this.damage = 5 + this.strength;
+    }
+}
+class Archer extends Adventurer {}
+class Wizard extends Adventurer {}
 
-function createHero() {
-
+function createHero(name, gameClass, str, agi, wis) {
+    return new Warrior(name, gameClass, str, agi, wis);
 }
 
-function startBattle() {
+function startBattle(name, gameClass) {
+    let newSpawn = new Spawn();
+    let yourHero = createHero(name, gameClass, 7, 6, 6);
+    let currentRound = 0;
 
+    console.log(yourHero);
+    console.log(newSpawn);
+
+    while (newSpawn.hitpoints > 0 && yourHero.hitpoints > 0){
+        
+        // Start round
+        currentRound++;
+        console.log(`ROUND ${currentRound}, FIGHT!`);
+
+        // You attack spawn first
+        yourHero.attack(newSpawn);
+        console.log(`${yourHero.name} attacked with ${yourHero.damage}. Spawn has ${newSpawn.hitpoints}HP left.`);
+        
+        // Spawn attacks you
+        newSpawn.attack(yourHero);
+        console.log(`Spawn attacked with ${newSpawn.damage}. ${yourHero.name} has ${yourHero.hitpoints}HP left.`);
+
+    }
+
+    console.log(`BATTLE ENDED AFTER ${currentRound} ROUNDS! YOUR HP: ${yourHero.hitpoints}. SPAWN HP: ${newSpawn.hitpoints}`);
 }
+
+startBattle("Moonfury", "Warrior");
