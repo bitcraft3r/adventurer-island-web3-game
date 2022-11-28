c851329b89b7605337b830bf06accc12e625b125
onemanatatime <8282076+onemanatatime@users.noreply.github.com>
2022-11-28 23:46:37 +0800

    import and render collision boundaries

--- game-ui/index.js
+++ game-ui/index.js
@@ -4,6 +4,38 @@ const c = canvas.getContext("2d"); // canvas context
 canvas.width = 1024;
 canvas.height = 576;
 
+// create 2D array of collisions
+const collisionsMap = [];
+for (let i=0; i<collisions.length; i+=70){
+    collisionsMap.push(collisions.slice(i, 70 + i));
+}
+
+class Boundary {
+    static width = 48;
+    static height = 48;
+    constructor({position}){
+        this.position = position;
+        this.width = 48;
+        this.height = 48;
+    }
+    draw(){
+        c.fillStyle = 'red';
+        c.fillRect(this.position.x, this.position.y, this.width, this.height);
+    }
+}
+
+const boundaries = [];
+const offset = {
+    x: -735,
+    y: -650,
+}
+
+collisionsMap.forEach((row, i) => {
+    row.forEach((symbol, j) => {
+        if (symbol === 1025) boundaries.push(new Boundary({position: {x:j*Boundary.width+offset.x, y:i*Boundary.height+offset.y}}))
+    })
+})
+
 c.fillStyle = "grey";
 c.fillRect(0, 0, 1024, 576);
 
@@ -23,8 +55,10 @@ class Sprite {
     }
 }
 
+
+
 const background = new Sprite({ 
-    position: {x:-735, y:-600},
+    position: {x:offset.x, y:offset.y},
     image: bgImage,
 });
 
@@ -38,6 +72,10 @@ const keys = {
 function animate() {
     window.requestAnimationFrame(animate);
     background.draw();
+    // draw boundaries before player so player moves above boundaries
+    boundaries.forEach(boundary => {
+        boundary.draw();
+    })
     c.drawImage(
         playerImage, 
         0, // x coordinate to being cropping from
