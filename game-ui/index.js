9494af71e7027a03c43cc028c0233019324db297
onemanatatime <8282076+onemanatatime@users.noreply.github.com>
2022-11-29 00:09:30 +0800

    add collision detection

--- game-ui/index.js
+++ game-ui/index.js
@@ -46,16 +46,42 @@ const playerImage = new Image();
 playerImage.src = "./img/playerDown.png";
 
 class Sprite {
-    constructor({ position, image }){
+    constructor({ position, image, frames = {max:1} }){
         this.position = position;
         this.image = image;
+        this.frames = frames;
+
+        this.image.onload = () => {
+            this.width = this.image.width / this.frames.max;
+            this.height = this.image.height;
+            console.log(this.width);
+            console.log(this.height);
+
+        }
     }
     draw(){
-        c.drawImage(this.image, this.position.x, this.position.y);
+        c.drawImage(
+            this.image, 
+            0, // x coordinate to being cropping from
+            0, // y coordinate to begin cropping from
+            this.image.width/this.frames.max, // crop width
+            this.image.height, // crop height
+            this.position.x,
+            this.position.y,
+            this.image.width/this.frames.max, // size of output width
+            this.image.height, // size of output height
+            );
+        }
     }
-}
-
-
+    
+const player = new Sprite({
+    position: {
+        x: canvas.width/2 - 192/4/2, 
+        y: canvas.height/2 - 68/2,
+    },
+    image: playerImage,
+    frames: {max:4}
+})
 
 const background = new Sprite({ 
     position: {x:offset.x, y:offset.y},
@@ -69,29 +95,39 @@ const keys = {
     d: { pressed: false },
 }
 
+const movables = [background, ...boundaries]; // spread operator to take all items within the array, so there's no 2D arrays
+
+function rectangularCollision({rectangle1, rectangle2}){
+    return (
+        rectangle1.position.x + rectangle1.width >= rectangle2.position.x && // if right side of player > left side of box == colliding (on left side of box)
+        rectangle1.position.x <= rectangle2.position.x + rectangle2.width && // check left side of player vs right side of box
+        rectangle1.position.y <= rectangle2.position.y + rectangle2.height && // check top of player and bottom of box
+        rectangle1.position.y + rectangle1.height >= rectangle2.position.y // check bottom of player and top of box
+    )
+}
+
 function animate() {
     window.requestAnimationFrame(animate);
     background.draw();
     // draw boundaries before player so player moves above boundaries
     boundaries.forEach(boundary => {
         boundary.draw();
+        if ( rectangularCollision({rectangle1: player, rectangle2: boundary}) ){
+            console.log(`colliding`)
+        };
     })
-    c.drawImage(
-        playerImage, 
-        0, // x coordinate to being cropping from
-        0, // y coordinate to begin cropping from
-        playerImage.width/4, // crop width
-        playerImage.height, // crop height
-        canvas.width/2 - playerImage.width/4/2, 
-        canvas.height/2 - playerImage.height/2,
-        playerImage.width/4, // size of output width
-        playerImage.height, // size of output height
-    );
+    player.draw();
 
-    if (keys.w.pressed && lastKey === 'w') background.position.y += 5;
-    else if (keys.a.pressed && lastKey === 'a') background.position.x += 5;
-    else if (keys.s.pressed && lastKey === 's') background.position.y -= 5;
-    else if (keys.d.pressed && lastKey === 'd') background.position.x -= 5;
+
+    if (keys.w.pressed && lastKey === 'w'){
+        movables.forEach(movable => movable.position.y += 5)
+    } else if (keys.a.pressed && lastKey === 'a'){
+        movables.forEach(movable => movable.position.x += 5)
+    } else if (keys.s.pressed && lastKey === 's'){
+        movables.forEach(movable => movable.position.y -= 5)
+    } else if (keys.d.pressed && lastKey === 'd'){
+        movables.forEach(movable => movable.position.x -= 5)
+    } 
 }
 animate();
 
