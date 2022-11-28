8b20bbcba4c116961ff27d1b6ca4e20fef2bc310
onemanatatime <8282076+onemanatatime@users.noreply.github.com>
2022-11-28 22:49:43 +0800

    use Class for sprite creation

--- game-ui/index.js
+++ game-ui/index.js
@@ -7,15 +7,30 @@ canvas.height = 576;
 c.fillStyle = "grey";
 c.fillRect(0, 0, 1024, 576);
 
-const image = new Image();
-image.src = "./img/pelletTown.png";
+const bgImage = new Image();
+bgImage.src = "./img/pelletTown.png";
 
 const playerImage = new Image();
 playerImage.src = "./img/playerDown.png";
 
+class Sprite {
+    constructor({ position, image }){
+        this.position = position;
+        this.image = image;
+    }
+    draw(){
+        c.drawImage(this.image, -735, -600);
+    }
+}
+
+const background = new Sprite({ 
+    position: {x:-735, y:-600},
+    image: bgImage,
+});
+
 function animate() {
     window.requestAnimationFrame(animate);
-    c.drawImage(image, -735, -600);
+    background.draw();
     c.drawImage(
         playerImage, 
         0, // x coordinate to being cropping from
