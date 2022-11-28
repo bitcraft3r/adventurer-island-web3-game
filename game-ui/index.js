34ad569af833941b12ef09af5ee692f90a63bf64
onemanatatime <8282076+onemanatatime@users.noreply.github.com>
2022-11-28 22:20:05 +0800

    import and render map

--- game-ui/index.js
+++ game-ui/index.js
@@ -4,7 +4,12 @@ const c = canvas.getContext("2d"); // canvas context
 canvas.width = 1024;
 canvas.height = 576;
 
-console.log(c);
-
 c.fillStyle = "grey";
 c.fillRect(0, 0, 1024, 576);
+
+const image = new Image(); // Canvas api does not allow us to place image url directly within the image constructor, hence require next line
+image.src = "./img/pelletTown.png";
+
+image.onload = () => { // wait for background to load
+    c.drawImage(image, -725, -550);
+}
