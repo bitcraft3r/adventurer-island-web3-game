d66579ce70defc473009a4745b7c9bc9fea2df10
SOV3333 <8282076+sov3333@users.noreply.github.com>
2022-12-29 15:11:58 +0800

    add todo

--- react-website/src/containers/header/Header.jsx
+++ react-website/src/containers/header/Header.jsx
@@ -13,8 +13,12 @@ const Header = () => {
           </h1>
           <p>Unleash your inner adventurer and explore a vast, fantastical world on the blockchain. Join a team and work together to conquer quests, defeat monsters, and collect valuable treasure through this immersive and interactive game.</p>
           <div className="gpt3__header-content__input">
+            {/* TODO: Use Airtable Webhook to add new subscribers to Airtable
+            Webhook: https://hooks.airtable.com/workflows/v1/genericWebhook/appIxfq6Op2r5MWNj/wflpCbDRTufNWz80C/wtrbftfyKxg5c6frm
+            Tutorial: https://www.youtube.com/watch?v=TQJz2k9tT8c
+            */}
             <input type="email" placeholder="Your Email Address" />
-            <button type="button">Get Started</button>
+            <button type="button">Get Started</button> {/* onClick={`submit email input field's value to airtable webhook with method="POST"`} */}
           </div>
           <div className="gpt3__header-content__people">
             <img src={people} alt="people" />
