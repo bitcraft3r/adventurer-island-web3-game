71378e38c2f7d2c5d1471cdfb6e2cee072f46e6a
sov3333 <8282076+sov3333@users.noreply.github.com>
2023-01-11 17:02:01 +0800

    implement email subscribe using airtable webhook

--- react-website/src/containers/header/Header.jsx
+++ react-website/src/containers/header/Header.jsx
@@ -3,6 +3,19 @@ import './header.css';
 import people from '../../assets/people.png';
 import symbol from '../../assets/symbol.png';
 
+let webHook = "https://hooks.airtable.com/workflows/v1/genericWebhook/appIxfq6Op2r5MWNj/wflpCbDRTufNWz80C/wtrbftfyKxg5c6frm";
+
+const handleFormSubmit = (event) => {
+  // event.preventDefault();
+  console.log("form submitted");
+  try {
+    console.log(event.target.method);
+  } catch (error) {
+    console.log(event.target.method);
+    console.error(error);
+  }
+}
+
 const Header = () => {
   return (
     <div className="adv3__header section__padding" id="home">
@@ -12,14 +25,12 @@ const Header = () => {
 
           </h1>
           <p>Unleash your inner adventurer and explore a vast, fantastical world on the blockchain. Join a team and work together to conquer quests, defeat monsters, and collect valuable treasure through this immersive and interactive game.</p>
-          <div className="adv3__header-content__input">
-            {/* TODO: Use Airtable Webhook to add new subscribers to Airtable
-            Webhook: https://hooks.airtable.com/workflows/v1/genericWebhook/appIxfq6Op2r5MWNj/wflpCbDRTufNWz80C/wtrbftfyKxg5c6frm
-            Tutorial: https://www.youtube.com/watch?v=TQJz2k9tT8c
-            */}
-            <input type="email" placeholder="Your Email Address" />
-            <button type="button">Get Started</button> {/* onClick={`submit email input field's value to airtable webhook with method="POST"`} */}
-          </div>
+          {/* <div className="adv3__header-content__input"> */}
+            <form className="adv3__header-content__input" onSubmit={handleFormSubmit} action={webHook} method="POST">
+              <input type="email" id="email" name="email" placeholder="Your Email Address" />
+              <button type="submit">Get Started</button>
+            </form>
+          {/* </div> */}
           <div className="adv3__header-content__people">
             <img src={people} alt="people" />
             <p>1,600 people requested early access in the last 24 hours</p>
