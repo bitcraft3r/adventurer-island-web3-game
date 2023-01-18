3a74488000a40c509828ff9820027b7b655e72be
SOV <8282076+sov3333@users.noreply.github.com>
2023-01-18 15:01:24 +0800

    replace uni w sushi logo, update links to GEM

--- react-website/src/components/brand/Brand.jsx
+++ react-website/src/components/brand/Brand.jsx
@@ -1,17 +1,22 @@
 import React from 'react';
-import { uniswap, opensea, coinmarketcap, coingecko, dextools, arbitrum } from './imports';
+import { sushiswap, opensea, coinmarketcap, coingecko, dextools, arbitrum } from './imports';
 import './brand.css';
 
 const Brand = () => {
   return (
     <div className="adv3__brand section__padding">
       <div>
-        <a href="https://app.uniswap.org/#/swap?inputCurrency=ETH&outputCurrency=0xc4be0798e5b5b1c15eda36d9b2d8c1a60717fa92" target="_blank" rel="noopener noreferrer">
-          <img src={uniswap} alt="uniswap" />
+        <a href="https://app.sushi.com/swap?inputCurrency=ETH&outputCurrency=0x458a2df1A5C74C5dc9eD6E01Dd1178E6D353243B&chainId=42161" target="_blank" rel="noopener noreferrer">
+          <img src={sushiswap} alt="sushiswap" />
         </a>
       </div>
       <div>
-        <a href="https://opensea.io/collection/adventurer-eth" target="_blank" rel="noopener noreferrer">
+        <a href="https://www.dextools.io/app/en/arbitrum/pair-explorer/0xeca2c9d3881df18fb129582b65b82d67edd21d19" target="_blank" rel="noopener noreferrer">
+          <img src={dextools} alt="dextools" />
+        </a>
+      </div>
+      <div>
+        <a href="https://opensea.io/collection/adv3nturers" target="_blank" rel="noopener noreferrer">
           <img src={opensea} alt="opensea" />
         </a>
       </div>
@@ -23,11 +28,6 @@ const Brand = () => {
       <div>
         <a href="https://www.coingecko.com/en/coins/adventurer-gold" target="_blank" rel="noopener noreferrer">
           <img src={coingecko} alt="coingecko" />
-        </a>
-      </div>
-      <div>
-        <a href="https://www.dextools.io/app/en/arbitrum/pair-explorer/0x2297c88f8e505ce9f45a01d11e6448525b2d2e7e" target="_blank" rel="noopener noreferrer">
-          <img src={dextools} alt="dextools" />
         </a>
       </div>
       <div>
