2e2e887a2b56f86ac518f2e51dcf56542f4941cb
SOV <8282076+sov3333@users.noreply.github.com>
2023-01-18 15:04:28 +0800

    fix export sushiswap logo

--- react-website/src/components/brand/imports.js
+++ react-website/src/components/brand/imports.js
@@ -1,4 +1,4 @@
-import uniswap from '../../assets/uniswap.png';
+import sushiswap from '../../assets/sushiswap.png';
 import opensea from '../../assets/opensea.png';
 import coinmarketcap from '../../assets/coinmarketcap.png';
 import coingecko from '../../assets/coingecko.png';
@@ -6,7 +6,7 @@ import dextools from '../../assets/dextools.png';
 import arbitrum from '../../assets/arbitrum.png';
 
 export {
-    uniswap,
+    sushiswap,
     opensea,
     coinmarketcap,
     coingecko,
