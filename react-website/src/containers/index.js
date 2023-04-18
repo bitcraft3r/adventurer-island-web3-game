804443ecdd03647f8531508a9494dfe88d4af475
SOV3333 <8282076+sov3333@users.noreply.github.com>
2023-04-18 22:23:41 +0800

    org: clean up repo

--- react-website/src/containers/index.js
+++
@@ -1,6 +0,0 @@
-export { default as Features } from './features/Features';
-export { default as Footer } from './footer/Footer';
-export { default as Games } from './games/Games';
-export { default as Header } from './header/Header';
-export { default as Tokenomics } from './tokenomics/Tokenomics';
-export { default as Vision } from './vision/Vision';
